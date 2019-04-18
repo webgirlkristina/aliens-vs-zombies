47c1e5953fb6b0777a15883d7361ef38886d10e5
webgirlkristina <webgirlkristina@gmail.com>
2019-04-18 09:56:16 +0300

    Lesson 2: Gravity and jumps

--- Player.gd
+++ Player.gd
@@ -1,10 +1,15 @@
 extends KinematicBody2D
 
-const SPEED = 100
+const SPEED = 200
+const FLOOR = Vector2(0, -1)
+const GRAVITY = 970
+const JUMP_POWER = 500
 
 var velocity = Vector2()
 
 func _physics_process(delta):
+	if position.y > 800:
+		position.y = 0
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = SPEED
 		$Sprite.flip_h = false
@@ -14,4 +19,8 @@ func _physics_process(delta):
 	else:
 		velocity.x = 0
 	
-	move_and_slide(velocity)
+	if Input.is_action_pressed("ui_up") && is_on_floor():
+		velocity.y = -JUMP_POWER
+	
+	velocity.y += (GRAVITY * delta)
+	velocity = move_and_slide(velocity, FLOOR)
